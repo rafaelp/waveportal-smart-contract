2b55abfb044a694a1041112fcc824e2ad0db79ce
Rafael Lima <contato@rafael.adm.br>
2021-11-21 18:36:46 -0300

    Section 4 Lesson 2

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -58,7 +58,7 @@ contract WavePortal {
          * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
          */
         require(
-            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
             "Wait 15m"
         );
 
